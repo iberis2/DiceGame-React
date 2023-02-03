260c1830cfe658da8dd3f277801c60e49768fba3
iberis2 <snow9402@gmail.com>
2023-02-03 16:00:51 +0900

    버튼 스타일 변경

--- src/App.js
+++ src/App.js
@@ -26,8 +26,12 @@ function App() {
   return (
     <Fragment>
       <div>
-        <Button onClick={handleRollClick}>던지기</Button>
-        <Button onClick={handleClearClick}>처음부터</Button>
+        <Button color={"blue"} onClick={handleRollClick}>
+          던지기
+        </Button>
+        <Button color={"red"} onClick={handleClearClick}>
+          처음부터
+        </Button>
       </div>
       <div>
         <Board name="나" color="blue" gameHistory={myHistory} />
